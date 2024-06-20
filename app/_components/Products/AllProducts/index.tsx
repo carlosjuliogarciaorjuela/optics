eb1988fd1fd9c288216593a5d0d5aabe676ddf89
carlosjuliogarciaorjuela <cjgarcia655@soy.sena.edu.co>
2024-06-20 14:16:07 -0500

    change returning initial design

--- app/_components/Products/AllProducts/index.tsx
+++ app/_components/Products/AllProducts/index.tsx
@@ -1,5 +1,50 @@
+// // components/AllProducts.tsx
+// import Image from "next/image";
+// import { useEffect, useState } from "react";
+// import { getAllProducts, Product as ProductType } from "@/lib/shopify";
+//
+// const AllProducts = () => {
+//   const [products, setProducts] = useState<ProductType[]>([]);
+//
+//   useEffect(() => {
+//     async function fetchData() {
+//       const response = await getAllProducts();
+//       setProducts(response.body.data.products.edges);
+//     }
+//     fetchData();
+//   }, []);
+//
+//   return (
+//     <div className="grid h-72 lg:h-[400px] xl:h-[500px] w-[70%] gap-10 grid-cols-2 mt-10">
+//       {products.map((product) => (
+//         <div
+//           key={product.node.id}
+//           className="bg-white border rounded-lg border-gray-300 flex items-center justify-center flex-col"
+//         >
+//           <p className="lg:text-2xl text-lg text-center font-bold">
+//             {product.node.title}
+//           </p>
+//           <Image
+//             className="self-center w-[40%]"
+//             src={product.node.image.src}
+//             alt={product.node.title}
+//             width={500}
+//             height={500}
+//           />
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+//
+// export default AllProducts;
+
 import Image from "next/image";
-import image1 from "../../../../public/images/product1.png";
+
+import image1 from "@/public/images/Product01.png";
+import image2 from "@/public/images/Product02.png";
+import image3 from "@/public/images/Product03.png";
+import image4 from "@/public/images/Product04.png";
 
 const otherProducts = [
   {
@@ -8,15 +53,15 @@ const otherProducts = [
   },
   {
     title: "Producto 2",
-    image: image1,
+    image: image2,
   },
   {
     title: "Producto 3",
-    image: image1,
+    image: image3,
   },
   {
     title: "Producto 4",
-    image: image1,
+    image: image4,
   },
 ];
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,67 @@
+// "use client";
+//
+// import { useEffect, useState } from "react";
+// import { getAllProducts, Product } from "@/lib/shopify"; // Asegúrate de importar el tipo Product
+//
+// import CarouselComponent from "./_components/Carousel/Carousel";
+// import CardContainer from "@/app/_components/StoreCard/CardContainer";
+// import ProductComponent from "./_components/Products/Product";
+// import AllProducts from "./_components/Products/AllProducts";
+// import VideoPlayer from "@/app/_components/VideoPlayer/VideoPlayer";
+// import TabComponent from "@/app/_components/AboutUs/TabComponent";
+// import OrderForm from "@/app/_components/OrderForm/OrderForm";
+// import "dotenv/config";
+//
+// // Importa ProductPage
+// import ProductPage from "@/app/_components/ProductPage";
+//
+// export default function Home() {
+//   const [products, setProducts] = useState<Product[]>([]); // Especifica el tipo Product[]
+//
+//   useEffect(() => {
+//     async function fetchData() {
+//       const response = await getAllProducts();
+//       setProducts(response.body.data.products.edges);
+//     }
+//     fetchData();
+//   }, []);
+//
+//   return (
+//     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center">
+//       <div className="w-full p-0">
+//         <CarouselComponent />
+//       </div>
+//       <div className="w-full">
+//         <TabComponent />
+//       </div>
+//       <div className="flex justify-center items-center min-h-screen bg-gray-100">
+//         <CardContainer />
+//       </div>
+//       <div className="w-full my-10 flex justify-center">
+//         <VideoPlayer videoSrc="https://www.youtube.com/embed/R6wcLvotbEI?si=8uC5R5cV2KLqyQM9" />
+//       </div>
+//       <div className="flex flex-col items-center">
+//         <p className="text-3xl text-center mb-10 font-bold">
+//           Nuestros productos
+//         </p>
+//         <div>
+//           {products.map((product) => (
+//             <div key={product.node.id}>
+//               <h2>{product.node.title}</h2>
+//               <p>{product.node.description}</p>
+//               <img src={product.node.image.src} alt={product.node.title} />
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//       <div className="w-full flex justify-center p-8">
+//         <OrderForm />
+//       </div>
+//       {/* Agrega ProductPage dentro de Home */}
+//       <ProductPage />
+//     </div>
+//   );
+// }
 import CarouselComponent from "./_components/Carousel/Carousel";
 import CardContainer from "@/app/_components/StoreCard/CardContainer";
 import Product from "./_components/Products/Product";
