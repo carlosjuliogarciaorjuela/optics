fe8ff0a061c5b6aeef930fb6136b115ac49ddac9
carlosjuliogarciaorjuela <cjgarcia655@soy.sena.edu.co>
2024-06-27 08:33:48 -0500

    Change: Color bg pedidos

--- app/page.tsx
+++ app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
         </p>
         <AllProducts />
       </div>
-      <div className="w-full flex justify-center p-8 bg-PastelGreen">
+      <div className="w-full flex justify-center p-8 bg-navyblue">
         <OrderForm />
       </div>
     </div>
